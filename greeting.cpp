2d28f6321c4bd2d06b27f29a9974d0f0ae8a7d51
Gemma-B <gemma@bertain.net>
2016-11-20 22:50:09 +0000

    i dont even know anymore

---
+++ greeting.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+int main() {
+    std::cout << "What is your name?" << std::endl;
+    std::string name;
+    std::cin >> name;
+    std::cout << "AHHH hello " << name << ". How old are you?" << std::endl;
+    double age;
+    std::cin >> age;
+    std::cout << "So your name is " << name << " and you are " << age << " years old." << std::endl;
+    return 0;
+}
+# HEYO-IT-C-NOW-1
