a53661d5bff775a159113e30b30ebacf481555f1
Gemma-B <gemma@bertain.net>
2016-11-23 01:26:49 +0000

    boi there are errors that i dont know how to fix so ask for help

--- main.cpp
+++ main.cpp
@@ -9,15 +9,18 @@ int main() {
         if (choice == drive) {
             std::cout << "where would you like to move to? choose position 0-7" << std::endl;
             std::cin >> distance;
-            if (0 <= distance && distance <= 7){
-                std::cout << "the robot is now in position" << distance << std::endl;
+            if (0 <= distance && distance <= 7) {
+                std::cout << "The robot is now in position " << distance << std::endl;
                 position = distance;
-            } else {
-                std:: << "try again this time with a value of 0-7" << std::endl;
+            };
+            else {
+                std::cout << "try again this time with a value of 0-7" << std::endl;
             };
         };
-        if (choice != drive) {
+        
+        else {
             std::cout << "not finished yet" << std::endl;
-        }
+        };
     return 0;
+
 };
