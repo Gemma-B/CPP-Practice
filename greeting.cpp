bbf3bbbe8af56f8484f66b13162a8522c77e9aa7
Gemma-B <gemma@bertain.net>
2016-11-30 07:59:06 +0000

    boy i fucked up the instructions so now im fidinfg them

--- greeting.cpp
+++ greeting.cpp
@@ -6,7 +6,29 @@ int main() {
     std::cout << "AHHH hello " << name << ". How old are you?" << std::endl;
     double age;
     std::cin >> age;
-    std::cout << "So your name is " << name << " and you are " << age << " years old." << std::endl;
+    if ( 7 >= age ) {
+        std::cout << "I doubt you can even read, let alone type" << std::endl;
+    }
+    else if ( 7 < age && age <= 13) {
+        std::cout << "have fun while you can" << std::endl;
+    }
+    else if ( 13 < age && age <= 20 ) {
+        std::cout << "You're a teenager, cool" << std::endl;
+    }
+    else if ( 20 < age && age <= 35 ) {
+        std::cout << " Hey, hows adult life treating you? How are taxes?" << std::endl;
+    }
+    else if ( 35 < age && age <= 60 ) {
+        std::cout << " You still got time" << std::endl;
+    }
+    else if ( 60 < age && age <= 100 ) {
+        std::cout << " the clock is ticking " << std::endl;
+    }
+    else if ( 100 < age && age <= 120) {
+        std::cout << " really hanging on by a thread there friend. " << std::endl;
+    }
+    else {
+        std::cout << " if you're human, then you're lying about your age" << std::endl;
+    }
     return 0;
-}
-# HEYO-IT-C-NOW-1
+    }
