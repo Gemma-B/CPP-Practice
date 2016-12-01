884150ccbfc85cb9828da52c0fb3fdb893bc67a1
Gemma-B <gemma@bertain.net>
2016-12-01 15:53:19 +0000

    sorted stuff started

---
+++ Day3/sorted.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+int main {
+    std::cout << " Input numbers! Enter anything that isn't a number to end your inputs " << std::endl;
+    double numbers;
+    std::cin >> numbers;
+    // Something with a while loop for when the inputs should stop. 
+    // append the inputs into a dynamic array inside the while loop
+    // outside of the loop used std::sorted or something? mayber maps? to sort it 
+    // print the sorted array! nice
+   /* while ( numbers == doubles ) {
+        std::cout << "good" << std::endl;
+    }*/
+    return 0;
+}
